24d20c11b69d5540fb4a94df96a5f6e80c2a2a4b
zhengzhiwen <37207747+zhengzhiw@users.noreply.github.com>
2020-03-30 15:03:19 +0800

    Create mock.vue

---
+++ mock.vue
@@ -0,0 +1,22 @@
+...
+...
+...
+export default{
+  name: 'mock',
+  data() {
+    return{
+      ...
+    }
+  },
+  methods: {
+    mockjs () {
+      this.$axios.get("/api/mock").then(function (ret){
+        if(Object.is(ret.status, 200)){
+          console.log(ret.data)
+        }
+      }).catch(functhion (err){
+        console.log(err)
+      })
+    }
+  }
+}
